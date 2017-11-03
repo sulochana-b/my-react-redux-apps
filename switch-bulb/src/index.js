a9762d60337651445e94662497389b4f745fac0c
Sulochana K <sulojana.k@gmail.com>
2017-11-03 09:16:06 +0530

    started working on switch-bulb app

---
+++ switch-bulb/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+import App from './container/App';
+
+import {Provider} from 'react-redux';
+import './index.css';
+
+const store = createStore(allReducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+registerServiceWorker();
+
+
+
+
+
+
+
+
+
+
+
+
+
